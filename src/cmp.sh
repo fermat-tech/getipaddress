f320ecbd87de94bea7ecc1d603ddc827621f688d
me <me@there.com>
2021-04-16 14:35:34 -0400

    First commit

---
+++ src/cmp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cmake_build_type=Release
+
+build_dir="../build"
+
+if [[ ! -d "${build_dir}" ]]
+then
+    mkdir -m 0700 "${build_dir}"
+fi
+
+cd "${build_dir}" &&
+
+cmake \
+    -DCMAKE_C_COMPILER=gcc \
+    -DCMAKE_CXX_COMPILER=g++ \
+    -DCMAKE_BUILD_TYPE="$cmake_build_type" \
+    ../src &&
+
+cmake --build .
---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <filesystem>
+
+namespace // Anonymous namespace
+{
+
+auto program_name = std::string{};
+bool do_work(int argc_, char **argv_);
+
+} // Anonymous namespace
+
+int
+main(int argc_, char **argv_)
+{
+    auto exit_code = 0;
+    {
+        using std::filesystem::path;
+        program_name = path{ *(argv_) }.stem().generic_string();
+    }
+    if (argc_ < 2)
+    {
+        std::cerr << "ERROR: Missing hostname arguments." << '\n';
+        std::exit(1);
+    }
+    do_work(argc_ - 1, argv_ + 1);
+    std::exit(exit_code);
+}
+
+namespace // Anonymous namespace
+{
+
+bool do_work(int argc_, char **argv_)
+{
+    auto success = true;
+    for (auto i = 0; i < argc_; ++i)
+    {
+        std::string hostname = *(argv_ + i);
+        std::cout << hostname << '\n';
+    }
+    return success;
+}
+
+}
